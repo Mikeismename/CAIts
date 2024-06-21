e0a4c1b807d991fafcb5e4cf278b148843f0f72d
Aleksandra Topalidi <70717995+Enc0der@users.noreply.github.com>
2024-06-21 15:24:49 +0500

    Update cleaning_data.py

--- data/cleaning_data.py
+++ data/cleaning_data.py
@@ -1,17 +1,96 @@
 import os
 import hashlib
-import json
 from bs4 import BeautifulSoup, Comment
 import unicodedata
 from warcio.archiveiterator import ArchiveIterator
 import re
-import shutil
+import csv
+from sudachipy import tokenizer, dictionary
+from datetime import datetime
+from fuzzywuzzy import fuzz
 
-# Загрузка конфигурации
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
+tokenizer_obj = dictionary.Dictionary().create()
+mode = tokenizer.Tokenizer.SplitMode.C
 
-# Функция очистки HTML контента и извлечения ссылок
+def mark_proper_nouns(text):
+    tokens = tokenizer_obj.tokenize(text, mode)
+    marked_text = ""
+    for token in tokens:
+        if token.part_of_speech()[0] == '名詞' and token.part_of_speech()[1] == '固有名詞':
+            marked_text += f"<NE>{token.surface()}</NE>"
+        else:
+            marked_text += token.surface()
+    return marked_text
+
+def remove_duplicates_and_keep_last(texts, threshold=90):
+    unique_texts = []
+    seen_titles = set()
+    last_seen_index = {}
+
+    for i, text in enumerate(texts):
+        for seen in seen_titles:
+            if fuzz.ratio(text, seen) > threshold:
+                last_seen_index[seen] = i
+                break
+        seen_titles.add(text)
+    
+    skip_to_index = -1
+    for i, text in enumerate(texts):
+        if i <= skip_to_index:
+            continue
+        if text in last_seen_index:
+            skip_to_index = last_seen_index[text]
+        unique_texts.append(text)
+    
+    return unique_texts
+
+
+def extract_date(text):
+    date_pattern = r'(\d{4}年\d{2}月\d{2}日 \d{2}:\d{2})'
+    date_match = re.search(date_pattern, text)
+    
+    date = convert_date_format(date_match.group(1)) if date_match else 'Неизвестно'
+    remaining_text = re.sub(date_pattern, '', text, count=1) if date_match else text
+    
+    return date, remaining_text.strip()
+
+def remove_unwanted_phrases_from_end(text):
+    unwanted_patterns = [
+        r'<NE>Twitter</NE>\(公式アカウント\)',
+        r'<NE>Facebook</NE>\(ファンページ\)',
+        r'<NE>Google</NE>\+',
+        r'Ustream',
+        r'ニコニコ',
+        r'<NE>YouTube</NE>',
+        r'関連サービス',
+        r'livedoor',
+        r'livedoor ニュース',
+        r'© <NE>LINE</NE> Corporation',
+        r'ログイン',
+        r'ログインするアカウントをお選びください。',
+        r'以下のいずれかのアカウントでBLOGOSにログインすることができます。',
+        r'コメントを書き込むには',
+        r'FacebookID、TwitterID のいずれかで認証を行う必要があります。',
+        r'※livedoorIDでログインした場合、ご利用できるのはフォロー機能、マイページ機能、支持するボタンのみとなります。',
+        r'<NE>twitter</NE> ID',
+        r'<NE>facebook</NE> ID',
+        r'livedoor ID',
+        r'ログインしてBLOGOSをもっと便利に',
+        r'Tweet \d+ コメント'
+    ]
+
+    unwanted_patterns.sort(key=lambda x: len(x), reverse=True)
+
+    for pattern in unwanted_patterns:
+        text = re.sub(pattern, '', text)
+
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    return text
+
+def normalize_text(text):
+    return unicodedata.normalize('NFKC', text)
+
 def clean_html_and_extract_links(html_content):
     soup = BeautifulSoup(html_content, 'lxml')
     for script in soup(["script", "style", "footer", "header", "nav", "aside", "form"]):
@@ -25,78 +104,141 @@ def clean_html_and_extract_links(html_content):
     text = soup.get_text(separator=' ')
     return text, links
 
-# Функция генерации имени файла
-def generate_filename(url, index):
-    max_length = 100
-    filename = os.path.basename(url).replace('/', '_') + f"_{index}"
-    if len(filename) == 0:
-        filename = hashlib.md5(url.encode('utf-8')).hexdigest()
-    if len(filename) > max_length:
-        filename = hashlib.md5(url.encode('utf-8')).hexdigest() + f'_{index}.txt'
-    else:
-        filename = filename[:max_length] + f'_{index}.txt'
-    return filename
-
-# Функция нормализации текста
-def normalize_text(text):
-    return unicodedata.normalize('NFKC', text)
-
-# Функция очистки текста от повторяющихся ссылок и лишних пробелов
 def clean_text(text):
-    # Удаляем повторяющиеся ссылки
     text = re.sub(r'(?:(https?://)\S+(?=\s|$))(\1\S+)', r'\1', text)
-    # Удаляем лишние пробелы и пустые строки
-    text = os.linesep.join([s.strip() for s in text.splitlines() if s.strip()])
+    text = re.sub(r'\s+', ' ', text).strip()
     return text
 
-# Функция удаления ссылок на изображения
 def remove_image_links(text):
     img_pattern = r'\bhttps?://(?:\S+?\.jpg|.*?\.jpg(?:\?.*?)?)\b'
     return re.sub(img_pattern, '', text)
 
-# Функция обработки и сохранения данных
-def process_and_save_data(base_directory, target_directory):
-    seen_texts = set()
-    for root, dirs, files in os.walk(base_directory):
-        for dir_name in dirs:
-            source_dir = os.path.join(root, dir_name)
-            target_dir = os.path.join(target_directory, dir_name)
-            os.makedirs(target_dir, exist_ok=True)
-            
-            for file in os.listdir(source_dir):
-                file_path = os.path.join(source_dir, file)
-                if file.endswith('.warc'):
-                    with open(file_path, 'rb') as stream:
-                        for index, record in enumerate(ArchiveIterator(stream)):
-                            if record.rec_type == 'response' and 'text/html' in record.http_headers.get_header('Content-Type'):
-                                url = record.rec_headers.get_header('WARC-Target-URI')
-                                payload = record.content_stream().read()
-                                cleaned_text, links = clean_html_and_extract_links(payload.decode('utf-8', errors='ignore'))
-                                normalized_text = normalize_text(cleaned_text)
-                                cleaned_text = clean_text(normalized_text)
-                                final_text = remove_image_links(cleaned_text)
+def remove_multiple_links(links):
+    return links[:1]
 
-                                text_hash = hashlib.md5(final_text.encode('utf-8')).hexdigest()
-                                if text_hash in seen_texts:
-                                    continue
-                                seen_texts.add(text_hash)
+def convert_date_format(date_str):
+    try:
+        dt = datetime.strptime(date_str, '%Y年%m月%d日 %H:%M')
+        return dt.strftime('%Y-%m-%d %H:%M')
+    except ValueError:
+        return 'Неизвестно'
 
-                                output_file_name = generate_filename(url, index)
-                                if not output_file_name.endswith('.txt'):
-                                    output_file_name += '.txt'
-                                output_file_path = os.path.join(target_dir, output_file_name)
+def remove_text_in_square_brackets(text):
+    text = re.sub(r'\[[^\]]*\]', '', text)
+    return text.strip()
 
-                                with open(output_file_path, 'w', encoding='utf-8') as output_file:
-                                    output_file.write(final_text + "\n\nLinks:\n" + "\n".join(links))
-                                    print(f'Очищенный текст из {url} сохранен в {output_file_path}')
-
-# Основная функция
-def main():
-    base_directory = config.get('base_directory', '/private/tmp/unpacked_files')
-    target_directory = config.get('target_directory', '/Users/aleksandratopalidi/Desktop/HackatonProductAI/cleaned')
+def extract_date(text):
+    date_pattern = r'(\d{4}年\d{2}月\d{2}日 \d{2}:\d{2})'
+    date_match = re.search(date_pattern, text)
     
-    # Обработка и сохранение данных из WARC файлов
-    process_and_save_data(base_directory, target_directory)
+    date = convert_date_format(date_match.group(1)) if date_match else 'Неизвестно'
+    
+    text = re.sub(date_pattern, '', text)
+    
+    return date, text
+
+def remove_unwanted_sections(text):
+    patterns = [
+        r'モバイル版へ戻る[\s\S]*?(?=\s|$)',      
+        r'新規登録 / ログイン[\s\S]*?(?=\s|$)',  
+        r'新着記事[\s\S]*?(?=\s|$)',            
+        r'ピックアップ[\s\S]*?(?=\s|$)',        
+        r'ランキング[\s\S]*?(?=\s|$)',          
+        r'コメント[\s\S]*?(?=\s|$)',            
+        r'議論[\s\S]*?(?=\s|$)',                
+        r'ブロガー[\s\S]*?(?=\s|$)',            
+        r'政治家[\s\S]*?(?=\s|$)',             
+        r'アンケート[\s\S]*?(?=\s|$)',         
+        r'<NE>facebook</NE>[\s\S]*?(?=\s|$)',   
+        r'livedoor[\s\S]*?(?=\s|$)',            
+        r'<NE>twitter</NE>[\s\S]*?(?=\s|$)',    
+        r'ID[\s\S]*?(?=\s|$)',                  
+        r'TwitterID[\s\S]*?(?=\s|$)',           
+        r'google[\s\S]*?(?=\s|$)',              
+        r'BLOGOS[\s\S]*?(?=\s|$)',              
+        r'FacebookID[\s\S]*?(?=\s|$)',          
+        r'TwitterID[\s\S]*?(?=\s|$)',           
+        r'Facebook[\s\S]*?(?=\s|$)',            
+        r'facebook[\s\S]*?(?=\s|$)',            
+        r'<NE>facebook</NE>[\s\S]*?(?=\s|$)',   
+        r'<NE>facebook</NE>',                   
+        r'facebook',                            
+        r'FacebookID[\s\S]*?(?=\s|$)',          
+        r'TwitterID[\s\S]*?(?=\s|$)',  
+        r'Facebook[\s\S]*?(?=\s|$)',   
+    ]
+
+    for pattern in patterns:
+        text = re.sub(pattern, '', text, flags=re.IGNORECASE)
+
+    return text
+
+def process_and_save_data(base_directory, csv_file_path):
+    seen_texts = set()
+    counter = 0
+    
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['index', 'date', 'text', 'links']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        for file in os.listdir(base_directory):
+            file_path = os.path.join(base_directory, file)
+            if os.path.isfile(file_path) and file.endswith('.warc'):
+                print(f'Processing file: {file_path}')
+                with open(file_path, 'rb') as stream:
+                    for index, record in enumerate(ArchiveIterator(stream)):
+                        if record.rec_type == 'response' and 'text/html' in record.http_headers.get_header('Content-Type'):
+                            url = record.rec_headers.get_header('WARC-Target-URI')
+                            payload = record.content_stream().read()
+
+                            
+                            cleaned_text, links = clean_html_and_extract_links(payload.decode('utf-8', errors='ignore'))
+                            
+                            normalized_text = normalize_text(cleaned_text)
+                            
+                            date, text_for_marking = extract_date(normalized_text)
+                            
+                            marked_text = mark_proper_nouns(text_for_marking)
+
+                            
+                            cleaned_text = clean_text(marked_text)
+                            final_text = remove_image_links(cleaned_text)
+
+                            final_text = remove_text_in_square_brackets(final_text)
+
+                            links = remove_multiple_links(links)
+
+                            final_text = remove_unwanted_sections(final_text)
+                            
+                            final_text = remove_unwanted_phrases_from_end(final_text)
+
+                            lines = final_text.split('\n')
+                            final_text = '\n'.join(remove_duplicates_and_keep_last(lines))
+
+                            text_hash = hashlib.md5(final_text.encode('utf-8')).hexdigest()
+                            if text_hash in seen_texts:
+                                continue
+                            seen_texts.add(text_hash)
+
+                            writer.writerow({'index': counter, 'date': date, 'text': final_text, 'links': ','.join(links)})
+                            counter += 1
+
+                            print(f'Очищенный текст из {url} сохранен в CSV')
+
+
+def main():
+    base_directory = '/Users/aleksandratopalidi/Desktop/MLContest/Data'
+    target_directory = '/Users/aleksandratopalidi/Desktop/MLContest/Clickhouse'
+    subfolders = [ '2023']
+    
+    os.makedirs(target_directory, exist_ok=True)
+    
+    for folder in subfolders:
+        subfolder_path = os.path.join(base_directory, folder)
+        csv_file_path = os.path.join(target_directory, f'{folder}.csv')
+        process_and_save_data(subfolder_path, csv_file_path)
 
 if __name__ == '__main__':
     main()
+
