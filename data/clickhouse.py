807720abe0b81206ab3918dfa879106f76a5079d
Mikeismename <michael.topalidi@gmail.com>
2024-06-21 20:09:33 +0500

    Update clickhouse.py

--- data/clickhouse.py
+++ data/clickhouse.py
@@ -1,27 +1,60 @@
 import pandas as pd
 from clickhouse_driver import Client
+from transformers import BertTokenizer, BertModel
+import torch
+import numpy as np
+
 
 host = 'localhost'
 user = 'default'
 password = ''
 database = 'mydatabase'
 
+
 client = Client(host=host, user=user, password=password, database=database)
 
-file_path = 'embeddings.csv'
-data = pd.read_csv(file_path)
+# Инициализация токенизатора и модели
+tokenizer = BertTokenizer.from_pretrained('yandex/yandex-gpt')  
+model = BertModel.from_pretrained('yandex/yandex-gpt')
 
-new_columns = [f'column{i+1}' for i in range(len(data.columns))]
-data.columns = new_columns
+def get_embeddings(text):
+    inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    return outputs.last_hidden_state.mean(dim=1).numpy()[0]
 
-columns = data.columns.tolist()
-print("Columns:", columns)  # Вывод имен столбцов для проверки
+def search_clickhouse(embedding, time):
 
-insert_query = f"INSERT INTO mydatabase.embeddings ({', '.join(columns)}) VALUES"
+    query_time = f"SELECT * FROM embeddings WHERE time = '{time}'"
+    results_time = client.execute(query_time)
+    
+    if results_time:
 
-batch_size = 1000  # Размер пакета
-for i in range(0, len(data), batch_size):
-    batch = data.iloc[i:i + batch_size]
-    client.execute(insert_query, [tuple(row) for row in batch.values])
+        results = []
+        for row in results_time:
+            db_embedding = np.array(row[1:385]) 
+            similarity = np.dot(embedding, db_embedding) / (np.linalg.norm(embedding) * np.linalg.norm(db_embedding))
+            results.append((similarity, row))
 
-print("Данные успешно отправлены в ClickHouse")
+        results.sort(key=lambda x: x[0], reverse=True)
+        
+        if results:
+            # Возврат самой схожей строки и ее URL
+            best_match = results[0][1]
+            url = best_match[-1]  
+            return url
+
+    return None
+
+def main(query, time):
+    embedding = get_embeddings(query)
+    url = search_clickhouse(embedding, time)
+    if url:
+        print(f"Found URL: {url}")
+    else:
+        print("No matching documents found.")
+
+if __name__ == "__main__":
+    query = "やはがきによる人架空請求誕||硬!dたにコモど全ルルコジアルルアたレア"
+    time = "2023-06-21"  
+    main(query, time)
